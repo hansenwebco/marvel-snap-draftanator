c03701b11dfe362588f68eaab8edb4ca14be5735
Mark Hansen <mhansen@accusoft.com>
2023-02-24 17:26:06 -0500

    Basic building a deck working for arena

--- src/routes/components/deck-view.svelte
+++ src/routes/components/deck-view.svelte
@@ -1,15 +1,33 @@
 <script>
-  
-    import { DECK } from '$lib/store.js';
+	import { get } from 'svelte/store';
+	import { API_URL } from '$lib/store.js';
+	import { DECK } from '$lib/store.js';
 
-    let cards;
-    DECK.subscribe(c => {
-        cards = c
-    })
-    
+	let cdn = get(API_URL);
+	console.log(cdn);
+	let cards;
+	DECK.subscribe((c) => {
+		cards = c;
+	});
 </script>
 
-{#each cards as card}
-    {card.name} -
-{/each}
-<p>cards {cards.length}</p>
+<div id="deck-view">
+    {#each Array(12) as _, index (index)}
+        {#if index < cards.length}
+           <img id="card{index+1}" class="cards" alt="" src="{cdn}/images/cards/{cards[index].id}.webp">
+        {:else}
+        <img id="card{index+1}" class="cards" alt="Card 1" src="/images/deck-blank.png" />
+        {/if}
+    {/each}
+</div>
+
+<style>
+	.cards {
+		max-width: 92px;
+		padding: 4px;
+	}
+	#deck-view {
+		max-width: 600px;
+		margin: auto;
+	}
+</style>
