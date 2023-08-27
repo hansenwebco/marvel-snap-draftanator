50acfff587a04a33d3394b3bce7540db8566b422
Mark Hansen <mhansen@accusoft.com>
2023-08-27 18:55:16 -0400

    Added previews per mode

--- src/routes/components/footer.svelte
+++ src/routes/components/footer.svelte
@@ -12,7 +12,7 @@
     <a href="https://discord.gg/xXh7kccEbe" target="_blank" rel="noreferrer">Discord</a> ●
 	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draft">Github</a> ●
 	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draft/issues">Bugs</a> ●
-	<span id="version">3.0.3</span>
+	<span id="version">3.0.4</span>
 </div>
 
 <style>
