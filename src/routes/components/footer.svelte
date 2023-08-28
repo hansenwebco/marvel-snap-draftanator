ec206c88723c52e3798048e271ffe3b6695b0b86
Mark Hansen <mhansen@accusoft.com>
2023-08-28 11:29:12 -0400

    Moved to new repo updated links

--- src/routes/components/footer.svelte
+++ src/routes/components/footer.svelte
@@ -10,9 +10,9 @@
     <a target="_blank" rel="noreferrer" href="https://mastodon.social/@stonedonkey">Mastodon</a>
 	<br /><br />
     <a href="https://discord.gg/xXh7kccEbe" target="_blank" rel="noreferrer">Discord</a> ●
-	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draft">Github</a> ●
-	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draft/issues">Bugs</a> ●
-	<span id="version">3.0.4</span>
+	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draftanator">Github</a> ●
+	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draftanator/issues">Bugs</a> ●
+	<span id="version">3.0.5</span>
 </div>
 
 <style>
