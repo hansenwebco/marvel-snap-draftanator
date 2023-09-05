84c68f7976c605530f92d6a27d635db5dd583dc5
Mark Hansen <mhansen@accusoft.com>
2023-09-05 10:59:01 -0400

    version bump

--- src/routes/components/footer.svelte
+++ src/routes/components/footer.svelte
@@ -12,7 +12,7 @@
     <a href="https://discord.gg/xXh7kccEbe" target="_blank" rel="noreferrer">Discord</a> ●
 	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draftanator">Github</a> ●
 	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draftanator/issues">Bugs</a> ●
-	<span id="version">3.0.5</span>
+	<span id="version">3.0.6</span>
 </div>
 
 <style>
