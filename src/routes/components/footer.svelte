7497f618a5c9641061e39ed768ad34cff6af0898
Mark Hansen <mhansen@accusoft.com>
2023-09-07 20:28:39 -0400

    Add automated version to footer

--- src/routes/components/footer.svelte
+++ src/routes/components/footer.svelte
@@ -1,26 +1,27 @@
-
 <script>
-
-
+	import { onMount } from 'svelte';
+    let version = "local";
+	onMount(async () => {
+        version = process.env.GITHUB_RUN_NUMBER;
+    });
 </script>
 
 <div id="footer">
-	Created by <a target="_blank" rel="noreferrer" href="https://www.stonedonkey.com">stonedonkey</a> ●
-	Follow on <a target="_blank" rel="noreferrer" href="https://twitch.tv/stonedonkey">Twitch</a> ● 
-    <a target="_blank" rel="noreferrer" href="https://mastodon.social/@stonedonkey">Mastodon</a>
+	Created by <a target="_blank" rel="noreferrer" href="https://www.stonedonkey.com">stonedonkey</a> ● Follow on <a target="_blank" rel="noreferrer" href="https://twitch.tv/stonedonkey">Twitch</a> ●
+	<a target="_blank" rel="noreferrer" href="https://mastodon.social/@stonedonkey">Mastodon</a>
 	<br /><br />
-    <a href="https://discord.gg/xXh7kccEbe" target="_blank" rel="noreferrer">Discord</a> ●
+	<a href="https://discord.gg/xXh7kccEbe" target="_blank" rel="noreferrer">Discord</a> ●
 	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draftanator">Github</a> ●
 	<a target="_blank" rel="noreferrer" href="https://github.com/hansenwebco/marvel-snap-draftanator/issues">Bugs</a> ●
-	<span id="version">3.0.6</span>
+	<span id="version">version: {version}</span>
 </div>
 
 <style>
-    #footer {
-        font-family: 'Orbitron', Arial, Helvetica, sans-serif;
-        margin-top: 60px;
-        font-size: 10px;
-        text-align: center;
-        margin-bottom: 50px;
-    }
+	#footer {
+		font-family: 'Orbitron', Arial, Helvetica, sans-serif;
+		margin-top: 60px;
+		font-size: 10px;
+		text-align: center;
+		margin-bottom: 50px;
+	}
 </style>
