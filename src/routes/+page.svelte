2ff51c1855d0124a4911c5debbe95eb2276728b7
Mark Hansen <mhansen@accusoft.com>
2023-11-15 16:53:26 -0500

    Test release

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,33 +2,41 @@
 	import Header from './components/header.svelte';
 	import Footer from './components/footer.svelte';
 	import SelectMode from './components/select-mode.svelte';
-    import News from './components/news.svelte'
+	import News from './components/news.svelte';
+	import posthog from 'posthog-js';
+    import { onMount } from 'svelte';
+
+	onMount(() => {
+		posthog.init('phc_wvClKxKDq14TZGU0LpUYP3gG6SvisU4oSviGQSNmrab');
+	    //posthog.identify('USER_ID'); // Optionally, identify the user
+		posthog.capture('Home Page Loaded');
+	});
 </script>
 
 <svelte:head>
-    <title>Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats</title>
-    <meta name="title" content="Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats">
-    <meta name="description" content="The original Marvel Snap site for drafting decks.  Mavel Snap Draftanator supports sealed and arena formats and even allows you to draft decks with friends!">
+	<title>Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats</title>
+	<meta name="title" content="Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats" />
+	<meta name="description" content="The original Marvel Snap site for drafting decks.  Mavel Snap Draftanator supports sealed and arena formats and even allows you to draft decks with friends!" />
 
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://www.marvelsnapdraft.com/">
-    <meta property="og:title" content="Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats">
-    <meta property="og:description" content="The original Marvel Snap site for drafting decks.  Mavel Snap Draftanator supports sealed and arena formats and even allows you to draft decks with friends!">
-    <meta property="og:image" content="https://www.marvelsnapdraft.com/images/preview.png">
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://www.marvelsnapdraft.com/" />
+	<meta property="og:title" content="Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats" />
+	<meta property="og:description" content="The original Marvel Snap site for drafting decks.  Mavel Snap Draftanator supports sealed and arena formats and even allows you to draft decks with friends!" />
+	<meta property="og:image" content="https://www.marvelsnapdraft.com/images/preview.png" />
 
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://www.marvelsnapdraft.com/">
-    <meta property="twitter:title" content="Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats">
-    <meta property="twitter:description" content="The original Marvel Snap site for drafting decks.  Mavel Snap Draftanator supports sealed and arena formats and even allows you to draft decks with friends!">
-    <meta property="twitter:image" content="https://www.marvelsnapdraft.com/images/preview.png">
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://www.marvelsnapdraft.com/" />
+	<meta property="twitter:title" content="Marvel Snap! Draftanator - Draft Marvel Snap Decks - Sealed and Arena Formats" />
+	<meta property="twitter:description" content="The original Marvel Snap site for drafting decks.  Mavel Snap Draftanator supports sealed and arena formats and even allows you to draft decks with friends!" />
+	<meta property="twitter:image" content="https://www.marvelsnapdraft.com/images/preview.png" />
 </svelte:head>
 
 <Header />
 
 <SelectMode />
 
-<News/>
+<News />
 
 <Footer />
